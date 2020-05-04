8350a376ca8c60895b14342145ee72a0d766f08c
k-saunagi <k5.daha55@gmail.com>
2020-05-04 11:31:50 +0900

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+  def update
+      @user = current_user
+    if @user.update(user_params)
+      flash[:success] = "You have updated user successfully."
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
 	private
 	def user_params
 		params.require(:user).permit(:name, :introduction, :profile_image)
